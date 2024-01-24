7a92d72573215bad322c1d3d9ad3476951d3e003
Adam Johnson <adaojohnson902@gmail.com>
2024-01-23 18:55:17 -0700

    removed some of the vite template html

--- webapp/components/HelloWorld.vue
+++ webapp/components/HelloWorld.vue
@@ -3,9 +3,6 @@ import { ref } from 'vue'
 import axios from 'axios';
 import MovieData from './MovieData.vue'
 
-defineProps({
-  msg: String,
-})
 
 let movie = ref('')
 let movieListData = ref()
@@ -21,10 +18,10 @@ const handleSubmit = async () => {
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
   <form @submit.prevent="handleSubmit">
-    <p>{{ movie }}</p>
-    <input v-model="movie" placeholder="Please enter a movie">
+    <h1>Adam's Movie Lookup</h1>
+    <p>This app will display 10 movies using The Movie Database API</p>
+    <input v-model="movie" placeholder="Please enter a movie title">
     <button type="submit">Search</button>
   </form>
   <MovieData v-if="movieListData" :movieListData="movieListData"/>
